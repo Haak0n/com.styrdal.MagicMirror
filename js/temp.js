717b6f161c6f5d09454af3014da7c587c8a7759c
Haak0n <styrdal@gmail.com>
2016-04-12 00:23:13 +0200

    test21312312

--- js/temp.js
+++ js/temp.js
@@ -6,11 +6,10 @@ function tempInit () {
 		url: 'gettemp.php',
 		datatype: 'json',
 		success: function(data) {
+			console.log(data);
 			var temp1 = data.temp.temp1,
 				temp2 = data.temp.temp2,
 				date = data.date;
-			
-			console.log(data);
 			
 			$("#intemp").html(temp1 + "&deg");
 			$("#outtemp").html(temp2 + "&deg");
