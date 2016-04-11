c5a104a68c7cb4956f49f3274221be5765da6524
Haak0n <styrdal@gmail.com>
2016-04-11 23:37:19 +0200

    gettemp4

--- gettemp.php
+++ gettemp.php
@@ -2,13 +2,21 @@
 $filename = "/home/pi/tempsens/templog";
 $db = new SQLite3($filename) or die('Unable to open database');
 
-$sql = "SELECT temp1, temp2, date FROM templog ORDER BY rowid LIMIT 1";
+$sql = "SELECT temp1, temp2, date FROM templog ORDER BY rowid DESC LIMIT 1";
 $ret = $db->query($sql);
 $row = $ret->fetchArray(SQLITE3_ASSOC);
 
 $temp1 = $row['temp1'];
 $temp2 = $row['temp2'];
-$date = $row['date'];
+
+$date = date_parse_from_format("j/mY H:i:s", $row['date']);
+$year = $dateparse['year'];
+$month = $dateparse['month'] - 1;
+$day = $dateparse['day'];
+$hour = $dateparse['hour'];
+$minute = $dateparse['minute'];
+$second = $dateparse['second'];
+
 
 $table = array('temp1' => $temp1, 'temp2' => $temp2, 'date' => $date);
 $jsontable = json_encode($table);
