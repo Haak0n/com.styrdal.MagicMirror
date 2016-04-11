b2ada7afe6b60e1c7305dfe0b041a7681a2444f0
Haak0n <styrdal@gmail.com>
2016-04-11 23:35:12 +0200

    gettemp3

--- gettemp.php
+++ gettemp.php
@@ -8,15 +8,7 @@ $row = $ret->fetchArray(SQLITE3_ASSOC);
 
 $temp1 = $row['temp1'];
 $temp2 = $row['temp2'];
-
-$dateparse = date_parse_from_format("j/mY H:i:s", $row['date']);
-$year = $dateparse['year'];
-$month = $dateparse['month'] - 1;
-$day = $dateparse['day'];
-$hour = $dateparse['hour'];
-$minute = $dateparse['minute'];
-$second = $dateparse['second'];
-$date = "Date(" .$year . ", " . $month . ", " . $day . ", " . $hour . ", " . $minute . ", " . $second . ")";
+$date = $row['date'];
 
 $table = array('temp1' => $temp1, 'temp2' => $temp2, 'date' => $date);
 $jsontable = json_encode($table);
