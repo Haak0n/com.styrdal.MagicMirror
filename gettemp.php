e980e455b6241385b614132fa969a6caecb07925
Haak0n <styrdal@gmail.com>
2016-04-11 23:40:46 +0200

    gettemp6

--- gettemp.php
+++ gettemp.php
@@ -9,13 +9,7 @@ $row = $ret->fetchArray(SQLITE3_ASSOC);
 $temp1 = $row['temp1'];
 $temp2 = $row['temp2'];
 
-$date = date_parse_from_format("j/mY H:i:s", $row['date']);
-$year = $date['year'];
-$month = $date['month'] - 1;
-$day = $date['day'];
-$hour = $date['hour'];
-$minute = $date['minute'];
-$second = $date['second'];
+$date =  $row['date'];
 
 
 $table = array('temp1' => $temp1, 'temp2' => $temp2, 'date' => $date);
